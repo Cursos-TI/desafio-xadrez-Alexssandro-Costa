93bbf63f08fa4efbbe3619b3b4f8142630718bd0
Alexssandro-Costa <165922452+Alexssandro-Costa@users.noreply.github.com>
2025-02-24 19:40:48 +0000

    versão final

--- xadrez.c
+++ xadrez.c
@@ -7,15 +7,19 @@ void movimentacao_bispo(int casas)
 {
     if(casas > 0)
     {
+        movimentacao_bispo(casas-1); // chamada a recursão
+
+        printf("\n%i° movimento\n", casas);
         for (int i = 0; i < 1; i++)
         { 
+            // mostra "cima" uma vez na tela
             puts("Cima");
             for(int j = 0; j < 1 ; j++)
             {
+                // mostra "direita" uma vez na tela
                 puts("direita");
             } 
         }
-        movimentacao_bispo(casas-1); // chamada a recursão
         
     }
 
@@ -27,6 +31,9 @@ void movimentacao_torre(int casas)
     if(casas > 0)
     {
         movimentacao_torre(casas-1); // chamada a recursão
+
+        // printa a movimentação na tela
+        printf("\n%i° movimento\n", casas);
         puts("direita");
     }
 }
@@ -37,6 +44,9 @@ void movimentacao_rainha(int casas)
     if(casas > 0)
     {
         movimentacao_rainha(casas-1); // chamada a recursão
+
+        // printa a movimentação na tela
+        printf("\n%i° movimento\n", casas);
         puts("Esquerda");
     }
 }
@@ -46,24 +56,26 @@ int main() {
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
     int movimento = 0;   // variavel que contem quantas casas que as peças irão se mover
-    char validacao;
+    char validacao[2];
+
     do
     {
         puts("Deseja mover as peça em quantas casas?\n escolha de 1 a 9:"); 
-        scanf(" %s", &validacao); // determina quantas casas a peça irá se mover
+        scanf("%1s", &validacao); // determina quantas casas a peça irá se mover
 
         // verifica se oque foi inserido é um digito 
-        if(isdigit(validacao))
+        if(isdigit(validacao[0]))
         {
-            if((validacao - '0') < 10 )
+            // verifica se oque foi inserido é um inteiro de 1 a 9
+            if((validacao[0] - '0') < 10 && (validacao[0] - '0') > 0 )
             {
-                movimento =  validacao - '0';
+                movimento =  validacao[0] - '0';
                 break;
             }
         }
-        puts("Digite um numero inteiro de 1 a 9\n");
+        puts("ERRO! por favor, Digite um numero inteiro de 1 a 9\n");
     
-    }while(movimento <= 0 || movimento > 10);
+    }while(1);
     
     // chamada do procedimento de movimentação do bispo
     puts("Movimentação do Bispo");
